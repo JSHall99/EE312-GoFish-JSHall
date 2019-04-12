815c7ad5d0226db43c907d59392705cc8ad8d5bc
JSHall99 <jackson.s.hall@gmail.com>
2019-04-11 22:11:08 -0500

    Completed more thorough testing of card.cpp

--- card_test.cpp
+++ card_test.cpp
@@ -9,7 +9,13 @@ using namespace std;
 
 int main(void)
 {
-   Card card1;
-   cout << "toString = " << card1.toString() << endl;
+   Card card1(11, Card::diamonds);
+   Card card2(5, Card::diamonds);
+   cout << "card1 = " << card1 << endl;
+   cout << "card2 = " << card2 << endl;
+   cout << "same suit? = " << card1.sameSuitAs(card2) << endl;
+   cout << "card1 rank = " << card1.getRank() << endl;
+   cout << "card1 == card2 " << (card1 == card2) << endl;
+   cout << "card1 != card2 " << (card1 != card2) << endl;
    return 0;
 }
